1afc8763d56dae64e6f5b34cc18e2d903db096c2
Shivanimayecha <shivanimayecha0908@gmail.com>
2018-09-20 12:16:23 +0530

    test

---
+++ app/src/main/java/com/example/aazad/renton/Utils.java
@@ -0,0 +1,19 @@
+package com.example.aazad.renton;
+
+/**
+ * Created by AAZAD on 26/07/2018.
+ */
+
+public class Utils {
+
+    //Email Validation pattern
+    //Email Validation pattern
+    public static final String regEx = "\\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,4}\\b";
+    //Fragments Tags
+
+   // public static final String regEx = "\"[a-zA-Z0-9._-]+@[a-z]+\\\\.+[a-z]+\"";
+
+    public static final String Login_Fragment = "Login_Fragment";
+    public static final String SignUp_Fragment = "SignUp_Fragment";
+    public static final String ForgotPassword_Fragment = "ForgotPassword_Fragment";
+}
